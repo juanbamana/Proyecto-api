1852c06338e2cb47a85f35121a0d9770e6d22f43
juanbamana <juanmana0402@gmail.com>
2023-05-26 11:18:25 +0200

    buscador

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,8 +1,9 @@
 const { restart } = require("nodemon")
 const validator = require('validator');
 const Articles = require('../models/Articles')
-const {validateArticle} = require('../helpers/validate')
-
+const { validateArticle } = require('../helpers/validate')
+const fs = require('fs');
+const path = require('path')
 const testing = (req, res) => {
 
     return res.status(200).json({
@@ -19,18 +20,18 @@ const create = (req, res) => {
 
     // validar datos
 
-    try{
+    try {
         validateArticle(params)
-    
-        } catch (err) {
-            return res.status(400).json({
-    
-                status: "error",
-                mensaje: "faltan datos"
-    
-            })
-        }
-    
+
+    } catch (err) {
+        return res.status(400).json({
+
+            status: "error",
+            mensaje: "faltan datos"
+
+        })
+    }
+
 
     // Crear el objeto a guardar
     const article = new Articles(params)
@@ -127,8 +128,8 @@ const editArticle = (req, res) => {
     let params = req.body
 
 
-    try{
-    validateArticle(params)
+    try {
+        validateArticle(params)
 
     } catch (err) {
         return res.status(400).json({
@@ -140,7 +141,7 @@ const editArticle = (req, res) => {
     }
 
 
-    Articles.findOneAndUpdate({ _id: idArticle }, params, {new: true})
+    Articles.findOneAndUpdate({ _id: idArticle }, params, { new: true })
         .then(function (result) {
             res.status(200).json({
                 status: "success",
@@ -157,6 +158,115 @@ const editArticle = (req, res) => {
 
 }
 
+const upload = (req, res) => {
+
+
+
+
+    let nameFile = req.file.originalname
+    let splitName = nameFile.split('\.')
+    let fileExtension = splitName[1]
+
+    if (fileExtension != 'png' && fileExtension != 'jpeg' && fileExtension != 'jpg') {
+
+
+        fs.unlink(req.file.path, (err) => {
+
+
+            return res.status(400).json({
+                status: 'error',
+                message: 'Formato invalido'
+
+            })
+        })
+    } else {
+
+        let idArticle = req.params.id
+
+
+
+
+        Articles.findOneAndUpdate({ _id: idArticle }, { imagen: req.file.filename }, { new: true })
+            .then(function (result) {
+                res.status(200).json({
+                    status: "success",
+                    mensaje: "Actualizado con exito",
+                    article: result,
+                    fichero: req.file
+
+                })
+            })
+            .catch(function (err) {
+                res.status(404).json({
+                    status: err,
+                    mensaje: "No se ha podido Actualizar",
+                    fichero: req.file
+                })
+            })
+
+
+
+
+    }
+
+
+}
+
+
+const image = (req, res) => {
+
+    let fichero = req.params.fichero
+    let route = './images/' + fichero;
+    fs.stat(route, (error, existe,) => {
+        if (existe) {
+            return res.sendFile(path.resolve(route))
+        } else {
+            return res.status(404).json({
+
+                status: 'error',
+                mensaje: "imagen no existe",
+                existe,
+                fichero,
+                route
+            })
+        }
+    })
+}
+
+
+const buscador = (req, res) => {
+    // Sacar el string de busqueda
+    let busqueda = req.params.busqueda;
+
+    // Find OR 
+    Articles.find({
+        "$or": [
+            { "titulo": { "$regex": busqueda, "$options": "i" } },
+            { "contenido": { "$regex": busqueda, "$options": "i" } },
+        ]
+    })
+        .sort({ fecha: -1 })
+        .exec()
+        .then(function (result) {
+            res.status(200).json({
+                status: "success",
+            articulos: result
+
+            })
+        })
+        .catch(function (err) {
+            res.status(404).json({
+                status: "error",
+                mensaje: "No se han encontrado artículos"
+            })
+        })
+
+
+}
+
+
+
+
 
 
 
@@ -170,5 +280,8 @@ module.exports = {
     getArticles,
     getOneArticle,
     removeArticle,
-    editArticle
+    editArticle,
+    upload,
+    image,
+    buscador
 }
--- routes/articles.js
+++ routes/articles.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router()
 
 const ArticleController = require("../controllers/articles")
+
+const multer = require('multer');
+
+
+const imageStorages = multer.diskStorage({
+
+    destination: function(req,file, cb){
+        cb(null, './images/')
+    },
+
+    filename: function(req,file, cb){
+        cb(null, 'article' + Date.now() + file.originalname)
+    }
+})
+
+const uploads = multer({storage: imageStorages})
 // Rutas Prueba
 
 
@@ -14,6 +30,9 @@ router.get("/articles", ArticleController.getArticles)
 router.get("/articles/:id", ArticleController.getOneArticle)
 router.delete("/articles/:id", ArticleController.removeArticle)
 router.put("/articles/:id", ArticleController.editArticle)
+router.post("/upload/:id", [uploads.single('file0')], ArticleController.upload)
+router.get("/imagen/:fichero", ArticleController.image)
+router.get("/buscar/:busqueda", ArticleController.buscador);
 
 
 module.exports = router;
